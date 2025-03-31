b6706bac839ab9d3e11b4a29e10b6e0247fbc27f
TuNA <nguyentu2392@gmail.com>
2025-03-31 14:51:21 +0700

    portfolio v2

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -30,7 +30,7 @@ export const DATA = {
   ],
   navbar: [
     { href: "/", icon: HomeIcon, label: "Home" },
-    { href: "/blog", icon: NotebookIcon, label: "Blog" },
+    // { href: "/blog", icon: NotebookIcon, label: "Blog" },
   ],
   contact: {
     email: "nguyentu2392@gmail.com",
